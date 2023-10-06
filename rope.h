5abceabca0bed53d3656273a5b616956b8ed9c27
MaelJansen <maeljansen2003@gmail.com>
2023-10-06 14:14:58 +0200

    Modifications du fichier rope.h

--- rope.h
+++ rope.h
@@ -2,27 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct Rope {
-} Rope;
+typedef struct Rope;
 
-void rope_len(Rope* rope){
-}
+void rope_len(Rope* rope);
 
-void rope_insert_at(){
-}
+void rope_insert_at();
 
-char* substring(int pos, int len, int c, char* base){
-}
+char* substring(int pos, int len, int c, char* base);
 
-char research(Rope* rope, long int i){
-}
+char research(Rope* rope, long int i);
 
-void recursive(char* base, Rope* rope){
-}
+void recursive(char* base, Rope* rope);
 
-Rope* rope_new(char* base){
-}
+Rope* rope_new(char* base);
 
-void rope_delete(Rope* rope){
-}
+void rope_delete(Rope* rope);
  
