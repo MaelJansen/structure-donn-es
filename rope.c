b6ee364d4cb654c2b43c18b797d6a61dce3aca40
MaelJansen <maeljansen2003@gmail.com>
2023-10-06 14:10:11 +0200

    Changement de rope.h pour qu'il ne contienne que les prototypes des fonctions

--- rope.c
+++ rope.c
@@ -1,16 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <rope.h>
 
 typedef struct Rope {
     char node_content;
     Rope* left;
     Rope* right;
-    int size;
+    int* size;
     unsigned pointer;
 } Rope;
 
-void rope_len(Rope rope){
+void rope_len(Rope* rope){
 
 }
 
@@ -18,32 +19,32 @@ void rope_insert_at(){
 
 }
 
-char substring(int pos, int len, int c, char base)
+char* substring(int pos, int len, int c, char* base)
 {
-    char substring = malloc(sizeof(base));
+    char* substring = malloc(sizeof(base));
     while (c < len) {
-        substring = string[pos + c-1];
+        substring = char[pos + c-1];
         c++;
     }
     substring = '\0';
     return substring;
 }
 
-void research(Rope rope, int i){
-    if (rope.size < i && rope.right != NULL){
-        return research(rope.right, i - rope.size);
-    } else if (rope.left != NULL)
+char research(Rope* rope, long int i){
+    if (rope->size < i && rope->right != NULL){
+        return research(rope->right, i - rope->size);
+    } else if (rope->left != NULL)
     {
-        return research(rope.left, i);
+        return research(rope->left, i);
     }
-    return rope.node_content;    
+    return rope->node_content;
 }
 
-void recurcive(char base, Rope* rope){
+void recursive(char* base, Rope* rope){
     if (strlen(base) > 7){
         /* If the lenght > 7 : divide the string into 2 and recall the function with the new parts on the different childs*/
-        recurcive(substring(0, strlen(base)/2, 0, base), rope.left)
-        recurcive(substring((strlen(base)/2)+1, strlen(base)/2, (strlen(base)/2)+1, base), rope.rigth)
+        recursive(substring(0, strlen(base)/2, 0, base), rope->left);
+        recursive(substring((strlen(base)/2)+1, strlen(base)/2, (strlen(base)/2)+1, base), rope->right);
     } else {
         Rope rope = (Rope*)malloc(sizeof(Rope));
         rope->node_content = base;
@@ -52,10 +53,11 @@ void recurcive(char base, Rope* rope){
 
 Rope* rope_new(char* base){
     Rope* root = (Rope*)malloc(sizeof(Rope));
-    recurcive(base, root);
+    recursive(base, root);
+    return root;
 }
 
-void rope_delete(Rope rope){
+void rope_delete(Rope* rope){
 
 }
  
--- rope.h
+++ rope.h
@@ -3,60 +3,26 @@
 #include <string.h>
 
 typedef struct Rope {
-    char node_content;
-    Rope* left;
-    Rope* right;
-    int* size;
-    unsigned pointer;
 } Rope;
 
 void rope_len(Rope* rope){
-
 }
 
 void rope_insert_at(){
-
 }
 
-char* substring(int pos, int len, int c, char* base)
-{
-    char* substring = malloc(sizeof(base));
-    while (c < len) {
-        substring = char[pos + c-1];
-        c++;
-    }
-    substring = '\0';
-    return substring;
+char* substring(int pos, int len, int c, char* base){
 }
 
 char research(Rope* rope, long int i){
-    if (rope->size < i && rope->right != NULL){
-        return research(rope->right, i - rope->size);
-    } else if (rope->left != NULL)
-    {
-        return research(rope->left, i);
-    }
-    return rope->node_content;
 }
 
 void recursive(char* base, Rope* rope){
-    if (strlen(base) > 7){
-        /* If the lenght > 7 : divide the string into 2 and recall the function with the new parts on the different childs*/
-        recursive(substring(0, strlen(base)/2, 0, base), rope->left);
-        recursive(substring((strlen(base)/2)+1, strlen(base)/2, (strlen(base)/2)+1, base), rope->right);
-    } else {
-        Rope rope = (Rope*)malloc(sizeof(Rope));
-        rope->node_content = base;
-    }
 }
 
 Rope* rope_new(char* base){
-    Rope* root = (Rope*)malloc(sizeof(Rope));
-    recursive(base, root);
-    return root;
 }
 
 void rope_delete(Rope* rope){
-
 }
  
