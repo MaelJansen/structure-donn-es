8b7e89fee89512f4689ba5d8cc5c2909c2bb6f37
MaelJansen <maeljansen2003@gmail.com>
2023-10-14 17:20:13 +0200

    Correction de bugs

--- rope.c
+++ rope.c
@@ -72,7 +72,8 @@ size_t rope_len(Rope* rope){
 }
 
 void assign_weight(Rope* rope){
-    int weight = 0; 
+    int weight = 0;
+    printf("*%s\r", rope->left->node_content->content);
     if (rope->left != NULL) {
         assign_weight(rope->left);
         if (rope->right != NULL){
