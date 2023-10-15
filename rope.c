b4aedf98e1aebe78c6e7b7028b52fbccf6911358
MaelJansen <maeljansen2003@gmail.com>
2023-10-15 17:32:23 +0200

    Correction de bugs

--- rope.c
+++ rope.c
@@ -48,9 +48,9 @@ Rope* recursive(char* base, int pos){
         .size = 0,
         .last = true
         };
+    printf("*%s\n \n \n", base);
     if (strlen(base) > 7){
         rope->last = false;
-        printf("*%s\n \n \n", base);
         rope->left = recursive(substring(0, strlen(base)/2, 0, base), pos);
         rope->right = recursive(substring((strlen(base)/2)+1, strlen(base)/2, (strlen(base)/2)+1, base), strlen(base)/2+1);
     }
