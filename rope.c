d63a777bc7392b241fb83abaccac75352729503f
MaelJansen <maeljansen2003@gmail.com>
2023-10-15 16:38:51 +0200

    Correction de bugs

--- rope.c
+++ rope.c
@@ -61,7 +61,7 @@ Rope* recursive(char* base, int pos){
 
 int check_left(Rope* rope){
     if (rope->last != true){
-        return strlen(rope->left->node_content->*content);
+        return check_left(rope->left);
     }
     return strlen(rope->node_content->content);
 }
